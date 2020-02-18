e0158e39faf1b743c39f2dc32abff8b553d785e7
Rafael Gomes <rafaelg1402@protonmail.com>
2020-02-18 10:32:50 +0000

    added solution for 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add (a,b)
+    total = a + b
+end
+
+def subtract (a,b)
+    total = a- b
+end
+
+def sum(arr)
+    if arr.empty?
+        total = 0 
+    elsif (!arr.empty? && arr.length < 2)
+        total = arr[0]
+    else 
+        total = 0
+        arr.each {|number| total+=number}
+        total
+    end
+end
