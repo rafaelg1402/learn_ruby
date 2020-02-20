58b05531a6f6522e9af799b8c7bf931c8cd544bb
Rafael Gomes <rafaelg1402@protonmail.com>
2020-02-20 16:20:14 +0000

    finished 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,21 @@
 #write your code here
+def echo(name)
+    name
+end
+
+def shout(name)
+    name.upcase
+end
+
+def repeat (name,num = 0)
+       arr = Array.new
+if num > 1
+       while num > 0 do
+        arr.push(name)
+        num -= 1
+        end
+        name = arr.join(' ')
+else
+    name + " " + name
+end
+end
