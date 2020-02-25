479cb4b6bd96480420e0e1d33f96cd63975e3d0a
Rafael Gomes <rafaelg1402@protonmail.com>
2020-02-25 09:55:55 +0000

    added solutions for 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -19,3 +19,40 @@ else
     name + " " + name
 end
 end
+
+def start_of_word(word, num)
+    num2 = 0
+    finalword = ''
+    if num > 1
+        while num > 0 do
+        finalword += word[num2]
+        num -= 1
+        num2 += 1
+        end
+        word = finalword
+    else
+        word[num - 1]
+        end
+end
+
+def first_word(words)
+    arr = Array.new
+    arr = words.split(' ')
+    words = arr[0]
+end
+
+def titleize(words)
+finalword = ''
+    finalword += words.split(' ').map(&:capitalize).join(' ')
+        finalword.gsub! 'And', 'and'
+        finalword.gsub! 'Over', 'over'
+        finalword.gsub! 'The', 'the'
+    arr = Array.new
+        arr = finalword.split(' ')
+        if arr[0].include?('the')
+        arr[0].replace('The')
+        finalword = arr.join(' ')
+    else
+        finalword = arr.join(' ')
+        end
+end
