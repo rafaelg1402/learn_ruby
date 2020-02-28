654d2fb8af8ede8f731939ad6af912e38c0d626b
Rafael Gomes <rafaelg1402@protonmail.com>
2020-02-28 09:30:16 +0000

    added solution for phoneme section

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -7,11 +7,13 @@ def translate(word)
     arr.map! {|x| if x.start_with?(/[aeiou]/) then x += 'ay' else x.replace x[1,x.length] + x[0] + 'ay' end }
     newword = arr.join(' ')
     elsif word.start_with?(/[aeiou]/) then word += 'ay'
+    elsif word[0].match(/[q]/) && word[1].match(/[u]/) then 
+    newword += word[2, word.length] + word[0] + word[1] + 'ay'
+    elsif !word[0].match(/[aeiou]/) && !word[1].match(/[aeiou]/) && !word[2].match(/[aeiou]/) then 
+    newword += word[3, word.length] + word[0] + word[1] + word[2] + 'ay'
     elsif !word[0].match(/[aeiou]/) && !word[1].match(/[aeiou]/) then 
     newword += word[2, word.length] + word[0] + word[1] + 'ay'
     else
         newword += word[1, word.length] + word[0] + 'ay'
      end
-
-    # arr.each {|a| if a.start_with?(/aeiou/) then puts 'hi' + a else 'poop' end}
 end
--- 04_pig_latin/pig_latin.rb~
+++ 04_pig_latin/pig_latin.rb~
@@ -1,1 +1,17 @@
 #write your code here
+def translate(word)
+    arr = Array.new
+        newword = ''
+    if word.match(' ') then 
+    arr = word.split(' ')
+    arr.map! {|x| if x.start_with?(/[aeiou]/) then x += 'ay' else x.replace x[1,x.length] + x[0] + 'ay' end }
+    newword = arr.join(' ')
+    elsif word.start_with?(/[aeiou]/) then word += 'ay'
+    elsif !word[0].match(/[aeiou]/) && !word[1].match(/[aeiou]/) then 
+    newword += word[2, word.length] + word[0] + word[1] + 'ay'
+    else
+        newword += word[1, word.length] + word[0] + 'ay'
+     end
+
+    # arr.each {|a| if a.start_with?(/aeiou/) then puts 'hi' + a else 'poop' end}
+end
